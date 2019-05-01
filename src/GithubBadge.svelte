ab9777225f082f18771d675c40010967827241ba
Дамјан Георгиевски <gdamjan@gmail.com>
2019-05-01 14:11:32 +0200

    GithubBadge component

---
+++ src/GithubBadge.svelte
@@ -0,0 +1,14 @@
+<style>
+  a {
+    position: absolute;
+    top: 0;
+    right: 20px;
+    border: 0;
+  }
+</style>
+
+<a href="https://github.com/irclogs/svelte">
+<img
+    src="./img/green-black.png"
+    alt="Fork me on GitHub" />
+</a>
