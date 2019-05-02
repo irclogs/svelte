57923edb8044b2c58d57adf42296abf7a62471f9
Дамјан Георгиевски <gdamjan@gmail.com>
2019-05-03 00:19:29 +0200

    Footer component

---
+++ src/Footer.svelte
@@ -0,0 +1,28 @@
+<script>
+  let online;
+</script>
+
+<style>
+  footer {
+    padding: 8px 6px 10px;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #efefef;
+    border-top: 1px dashed #aaaaaa;
+  }
+  footer a {
+    text-decoration: none;
+    color: #555555;
+  }
+  footer.red {
+      background-color: red;
+  }
+</style>
+
+<svelte:window bind:online={online}/>
+<footer class={online ? '':'red'}>
+  <a href="https://irc.softver.org.mk/">irclog home page</a>
+</footer>
+
