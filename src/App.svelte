f5457cc89b024781200a643dba197bb14df73aed
Дамјан Георгиевски <gdamjan@gmail.com>
2019-05-03 00:22:24 +0200

    finalize App

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,36 @@
 <script>
-  import { onMount } from 'svelte';
-  const url = new URL('https://irc.softver.org.mk/ddoc/_view/channel');
-  const params = new URLSearchParams({update_seq:true, reduce:true, group_level:1});
-
-  const headers = {'accept': 'application/json'};
-  let channels = [];
-  onMount(async () => {
-    const res = await fetch(url + '?' + params, {mode: "cors", headers:headers});
-    channels = (await res.json()).rows;
-    $: document.title = "irclog";
-  })
+  import Footer from './Footer.svelte';
+  import GithubBadge from './GithubBadge.svelte';
+  import HomePage from './HomePage.svelte';
+  import ChannelPage from './ChannelPage.svelte';
+  import { hashRouter } from './router.js';
+  const router = hashRouter();
 </script>
 
 <style>
-  ul { list-style-type: square; padding-left: 2em; margin: 1em 0px; }
+  :global(html) {
+    font-family: sans-serif;
+    font-size: 16pt;
+    line-height: 1.25;
+    height: 100%;
+  }
+  :global(body) {
+    margin: 0;
+    position: relative;
+    min-height: 100%;
+  }
+  div { padding: 20px; padding-bottom: 4em; }
 </style>
 
-<h1>IRC logs with realtime updates</h1>
+<div>
 
-<ul>
-	{#each channels as channel}
-    <li>{channel.key[0]}</li>
-	{:else}
-		<p>loading...</p>
-	{/each}
-</ul>
+{#if $router == ""}
+<HomePage/>
+{:else if $router == 404 }
+<HomePage error="not found"/>
+{:else}
+<ChannelPage name={$router}/>
+{/if}
+</div>
+<Footer/>
+<GithubBadge/>
