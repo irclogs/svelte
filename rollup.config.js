0810d4b8b070cc8d83b2d9398cfff7ca673f3682
Дамјан Георгиевски <gdamjan@gmail.com>
2020-05-07 00:02:33 +0200

    improve rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,45 @@
-import json from 'rollup-plugin-json';
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript';
-import { preprocess as ts_preprocess } from 'svelte-ts-preprocess';
+import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import svelte from "rollup-plugin-svelte";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import { preprocess as ts_preprocess } from "svelte-ts-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-    input: 'src/main.js',
-    output: {
-        sourcemap: true,
-        format: 'iife',
-        name: 'app',
-        file: 'dist/bundle.js'
-    },
-    plugins: [
-        svelte({
-            dev: !production,
-            css: css => {
-                css.write('dist/bundle.css');
-            },
-            preprocess: ts_preprocess
-        }),
-        json({
-           exclude: 'node_modules/**',
-           preferConst: true,
-        }),
+  input: "src/main.js",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "dist/bundle.js",
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: (css) => {
+        css.write("dist/bundle.css");
+      },
+      preprocess: ts_preprocess,
+    }),
+    json({
+      exclude: "node_modules/**",
+      preferConst: true,
+    }),
 
-        // If you have external dependencies installed from
-        // npm, you'll most likely need these plugins. In
-        // some cases you'll need additional configuration —
-        // consult the documentation for details:
-        // https://github.com/rollup/rollup-plugin-commonjs
-        resolve(),
-        commonjs(),
-        typescript({lib: ["es5", "es6", "dom"], target: "es5"}),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve(),
+    commonjs(),
+    typescript({ lib: ["es5", "es6", "dom"], target: "es5" }),
 
-        // If we're building for production (npm run build
-        // instead of npm run dev), minify
-        production && terser()
-    ]
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
+  ],
 };
