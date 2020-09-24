396077dccef05b823b660524639315f8da1bdea7
Дамјан Георгиевски <gdamjan@gmail.com>
2020-09-24 19:01:58 +0200

    update deps to latest versions

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import svelte from "rollup-plugin-svelte";
 import autoPreprocess from 'svelte-preprocess';
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
@@ -11,16 +11,16 @@ const production = !process.env.ROLLUP_WATCH;
 export default {
   input: "src/main.js",
   output: {
+    file: "dist/bundle.js",
     sourcemap: true,
     format: "iife",
     name: "app",
-    file: "dist/bundle.js",
   },
   plugins: [
     svelte({
       dev: !production,
       css: (css) => {
-        css.write("dist/bundle.css");
+        css.write("bundle.css");
       },
       preprocess: autoPreprocess(),
     }),
