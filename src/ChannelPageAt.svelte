f18c9f0b90470158c3d432f848ca52f7c99bf4ad
Дамјан Георгиевски <gdamjan@gmail.com>
2020-10-02 02:59:35 +0200

    avoid forced reactivity
    a component is properly reactive if the params are used in the body
    so make the body call a simple function.

--- src/ChannelPageAt.svelte
+++ src/ChannelPageAt.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
   // @ts-nocheck @ts-ignore-next-line: no typing for svelte-spa-router
   import { replace as redirect_to } from 'svelte-spa-router';
 
@@ -9,19 +8,15 @@
   import PageLoader from './spinners/PageLoader.svelte';
   import Button from './PaginationButton.svelte';
   import { getPage } from './couch';
-  import type { Page } from './couch';
 
   export let params: {channel: string, permalink: string };
 
-  let response: Promise<Page>;
-  $: {
-    let timestamp = oportunisticParsePemalink(params.permalink);
-    if (timestamp) {
-      response = getPage(params.channel, timestamp, 5);
-    } else {
-      // invalid permalink, redirect to 404
-      redirect_to('/404');
-    }
+  function load(channel, permalink, n) {
+    let timestamp = oportunisticParsePemalink(permalink);
+    if (!timestamp)
+      redirect_to('/404'); // invalid permalink, redirect to 404
+
+    return getPage(channel, timestamp, n);
   }
 </script>
 
@@ -29,7 +24,7 @@
 
 <Header><a href='#/'>irc logs</a> for <a href="#/{params.channel}">#{params.channel}</a></Header>
 
-{#await response}
+{#await load(params.channel, params.permalink, 10)}
   <PageLoader/>
 {:then page}
 
