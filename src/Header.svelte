a40167874039f925dba644f18fd0a31f610cfec6
Дамјан Георгиевски <gdamjan@gmail.com>
2020-10-06 12:48:20 +0200

    fix indentation and empty space

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,22 +1,20 @@
 <style>
-    header {
-      text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
-      color: #444444;
-      padding: 1em 0;
-    }
-    header h1 { margin: 0; }
-    header :global(h1 a) {
-      color: inherit;
-      text-decoration: none;
-    }
-    header :global(h1 a:hover) {
-      color: #777777;
-      text-decoration: underline;
-    }
-
-
+  header {
+    text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.3);
+    color: #444444;
+    padding: 1em 0;
+  }
+  header h1 { margin: 0; }
+  header :global(h1 a) {
+    color: inherit;
+    text-decoration: none;
+  }
+  header :global(h1 a:hover) {
+    color: #777777;
+    text-decoration: underline;
+  }
 </style>
 
 <header>
-    <h1><slot/></h1>
+  <h1><slot/></h1>
 </header>
