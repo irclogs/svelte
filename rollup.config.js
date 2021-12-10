b9ff2117b1fd2e0dd1faaf8139ec336ecdcec514
Дамјан Георгиевски <gdamjan@gmail.com>
2021-12-10 21:45:13 +0100

    expose build time process.env.NODE_ENV to code

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,24 @@ import css from "rollup-plugin-css-only";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import replace from '@rollup/plugin-replace';
 import svelte from "rollup-plugin-svelte";
 import autoPreprocess from 'svelte-preprocess';
 import { plugins as prodPlugins } from './rollup.prod.config.js';
 
-const production = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV == "production";
 const sourcemap = true;
 
 const plugins = [
+  replace({
+    values: {
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV ?? "development"),
+      __buildDate__: () => JSON.stringify(new Date()),
+    },
+    preventAssignment: true,
+  }),
   svelte({
-    compilerOptions: { dev: !production },
+    compilerOptions: { dev: !isProduction },
     extensions: [".svelte"],
     preprocess: autoPreprocess(),
   }),
@@ -23,12 +31,12 @@ const plugins = [
     preferConst: true,
   }),
 
-  resolve({browser: true, dedupe: ['svelte']}),
+  resolve({ browser: true, dedupe: ["svelte"] }),
   commonjs(),
 ];
 
-if (production) {
-   plugins.push(...prodPlugins);
+if (isProduction) {
+  plugins.push(...prodPlugins);
 }
 
 export default {
