abc7521411317cc9dffae3eeb32b7856fa212093
Дамјан Георгиевски <gdamjan@gmail.com>
2022-10-06 20:54:21 +0200

    get back these
    I shouldn't have removed them in 922d26ee2

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: process.env.npm_package_config_port },
+  build: { sourcemap: true },
   plugins: [svelte()]
 })
