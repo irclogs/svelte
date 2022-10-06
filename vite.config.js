370ea0634af693b331736a1541ca42974991a98b
Дамјан Георгиевски <gdamjan@gmail.com>
2022-10-06 18:59:00 +0200

    modernize build setup
    based on the latest scaffold as created by:
    `npm create vite@latest my-vue-app -- --template svelte-ts`
    
    update github actions to run on PRs too
    limit to run on push to main only

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import preprocess from "svelte-preprocess";
-
-export default defineConfig({
-  plugins: [svelte({ preprocess: preprocess() })],
-  server: { port: process.env.npm_package_config_port },
-  build: { sourcemap: true },
-});
