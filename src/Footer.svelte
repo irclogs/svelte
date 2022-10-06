147721b76512e69a92336284b03dc60a9185725a
Дамјан Георгиевски <gdamjan@gmail.com>
2022-10-06 19:50:54 +0200

    archive link in production build

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -5,7 +5,6 @@
 
 <svelte:window bind:online />
 <footer class:offline={!online}>
-  <a class="infolink" href="#" />
   <a href="https://irc.softver.org.mk/">irclog home page</a> | ver:
   <abbr title={import.meta.env.VITE_GITHUB_ARCHIVE_URL}>{version}</abbr>
 </footer>
@@ -24,43 +23,5 @@
   }
   footer.offline {
     border-top: 2px solid red;
-  }
-  .infolink:before {
-    content: "?";
-    display: inline-block;
-    font-family: sans-serif;
-    font-weight: bold;
-    text-align: center;
-    width: 1.8ex;
-    height: 1.8ex;
-    font-size: 1.4ex;
-    line-height: 1.8ex;
-    border-radius: 1.2ex;
-    margin-right: 4px;
-    padding: 1px;
-    color: blue;
-    background: white;
-    border: 1px solid blue;
-    text-decoration: none;
-  }
-
-  .infolink:hover:before {
-    color: white;
-    background: blue;
-    border-color: white;
-    text-decoration: none;
-  }
-
-  .dot {
-    display: inline-block;
-    width: 10px;
-    height: 10px;
-    border-radius: 100%;
-  }
-  .green {
-    background-color: rgb(34, 200, 162);
-  }
-  .red {
-    background-color: rgb(200, 34, 34);
   }
 </style>
