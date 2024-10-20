747c84d9683cc657ee23b1d8bb642f683313f20f
Дамјан Георгиевски <gdamjan@gmail.com>
2024-10-21 01:11:07 +0200

    simplify/reduce support files
    - merge `svelte.config.js` into `vite.config.ts`
    - remove `src/vite-env.d.ts` - not needed
    - remove `src/.babelrc.json` - not needed
    - tsconfig: noEmit, target: es2023

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-// https://vitejs.dev/config/
+/*
+ * Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+ * for more information about the svelte preprocessor
+ */
+const svelteConfig = {
+  preprocess: vitePreprocess(),
+  compilerOptions: {
+    runes: true,
+  },
+};
+
+
+/*
+ * https://vitejs.dev/config/
+ */
 export default defineConfig({
   server: { port: parseInt(process.env.npm_package_config_port) },
   build: { sourcemap: true },
-  plugins: [svelte()]
-})
+  plugins: [svelte(svelteConfig)],
+});
